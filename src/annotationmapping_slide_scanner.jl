a0b34fd7c79136ab5589bff43360629dd147efc5
Donghoon Lee <mdhe1248@gmail.com>
2023-01-04 19:23:52 -0500

    bug fix

--- src/annotationmapping_slide_scanner.jl
+++ src/annotationmapping_slide_scanner.jl
@@ -3,7 +3,7 @@
 """
 function annotation_boundary(annotation_img)
   parent_img = copy(parent(annotation_img))
-  output_img .= zeros(size(parent_img))
+  output_img = zeros(size(parent_img))
   for i in Base.axes(output_img,3)
     im1 = diff(parent_img[:,:,i], dims = 1)
     output_img[2:end, :, i] .+= im1
