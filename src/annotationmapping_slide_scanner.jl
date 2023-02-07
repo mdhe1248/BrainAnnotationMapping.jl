860299c7d080fcf4cc0676226543c9bc4b08d29a
Donghoon Lee <mdhe1248@gmail.com>
2023-02-07 17:30:29 -0500

    function added for slide scanner image analysis. (appendCol!)

--- src/annotationmapping_slide_scanner.jl
+++ src/annotationmapping_slide_scanner.jl
@@ -13,3 +13,29 @@ function annotation_boundary(annotation_img)
   output_img[output_img .!= 0] .= 1
   return(output_img)
 end
+
+"""
+append a column.
+
+`lbl` should be the id of brain structure.
+`dat` is corresponding value.
+empty values will have `missing`.
+"""
+function appendCol!(structures, lbls, dat, newcolname::Symbol)
+  if lenbth(lbls) .!= length(dat)
+    warn("Length of `lbls` and `dat` must be the same.")
+  end
+  v = fill(missing, size(structures,1))
+  v = convert(Vector{Union{Missing, Float64}}, v)
+  for (i, l) in enumerate(lbls)
+    idx = findfirst(structures.id .== l)
+    if isnothing(idx)
+      println(string(l, " does not exist."))
+    else
+      v[idx] = dat[i]
+    end
+  end
+  structures[!, newcolname] = v
+end
+
+
