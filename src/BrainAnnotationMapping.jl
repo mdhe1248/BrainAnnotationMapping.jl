bc86090f18102625c6f50f72af8f07618a2da380
Donghoon Lee <mdhe1248@gmail.com>
2023-03-23 10:17:44 -0400

    add functions to calculate pixel intensity, cfos counting, cfos amplitude

--- src/BrainAnnotationMapping.jl
+++ src/BrainAnnotationMapping.jl
@@ -1,6 +1,6 @@
 module BrainAnnotationMapping
 
-export retrieve, isdownstream, label_points, filter_points, count_cells, brainmapping, saveData, load_brainmap_json, annotation_boundary, appendCol!
+export retrieve, isdownstream, label_points, filter_points, count_cells, brainmapping, saveData, load_brainmap_json, annotation_boundary, appendCol!, appendVecCol!, getPosCounts, getIntensityMean, getNPixels, imshow_attn, selectArea, blob_filter
 # Write your package code here.
 
 include("annotationmapping_idisco.jl")
--- src/annotationmapping_slide_scanner.jl
+++ src/annotationmapping_slide_scanner.jl
@@ -38,4 +38,95 @@ function appendCol!(structures, lbls, dat, newcolname::Symbol)
   structures[!, newcolname] = v
 end
 
+"""
+append a column of Vector{Float64}.
+
+`lbl` should be the id of brain structure.
+`dat` is corresponding value.
+empty values will have `missing`.
+"""
+function appendVecCol!(structures, lbls, dat, newcolname::Symbol)
+  if length(lbls) .!= length(dat)
+    warn("Length of `lbls` and `dat` must be the same.")
+  end
+  v = fill(missing, size(structures,1))
+  v = convert(Vector{Union{Missing, Vector{Float64}}}, v)
+  for (i, l) in enumerate(lbls)
+    idx = findfirst(structures.id .== l)
+    if isnothing(idx)
+      println(string(l, " does not exist."))
+    else
+      v[idx] = dat[i]
+    end
+  end
+  structures[!, newcolname] = v
+end
+
+""" get cFos counts"""
+function getPosCounts(annotationimgw, pos, amp) 
+  ## Load image
+  lbls = Integer.(unique(annotationimgw))
+  c = zeros(Int, length(lbls))
+  a = [Vector{Float64}(undef,0) for i in eachindex(lbls)]
+  for (i, p) in enumerate(pos)
+    idx = findfirst(lbls .== annotationimgw[p...])
+    c[idx] = c[idx].+1
+    push!(a[idx], amp[i])
+  end
+  return(lbls, c, a)
+end
+
+""" get mean intensities in each brain area"""
+function getIntensityMean(fixedw, annotationimgw, img) 
+  ## Load image
+  lbls = Integer.(unique(annotationimgw))
+  meanintensities = [mean(img[annotationimgw .== lbl]) for lbl in lbls]
+  return(lbls, meanintensities)
+end
+
+function getIntensityMean(fixedw, annotationimgw, img, thresh) 
+  ## Load image
+  lbls = Integer.(unique(annotationimgw))
+  meanintensities = [mean(img[annotationimgw .== lbl .&& img .>= thresh]) for lbl in lbls]
+  return(lbls, meanintensities)
+end
+
+""" get number of pixels of each brain area"""
+function getNPixels(fixedw, annotationimgw, img) 
+  ## Load image
+  lbls = Integer.(unique(annotationimgw))
+  npixels = [length(img[annotationimgw .== lbl]) for lbl in lbls]
+  return(lbls, npixels)
+end
+
+""" overlap img and annotation boundary
+`c` is contrast limit
+"""
+function imshow_attn(annotationimgw, img, c)
+  ## Overage an image and annotation image
+  boundaryimg = annotation_boundary(annotationimgw)
+  scalefun = scaleminmax(c[1], c[2])
+  tmp = scalefun.(img)
+  tmp[boundaryimg .== 1] .= 1
+  guidict = ImageView.imshow(tmp)
+  guidict
+end
+
+""" select specific area"""
+function selectArea(attnimg, targetid)
+  output = copy(parent(attnimg))
+  output[output .!= targetid] .= 0
+  output
+end
+
+""" blob amplitude filter"""
+function blob_filter(blobs_log, thresh)
+  blobs_filtered = Vector{typeof(blobs_log[1])}(undef, 0)
+  for blob in blobs_log
+    if blob.amplitude > thresh
+      push!(blobs_filtered, blob)
+    end
+  end
+  blobs_filtered
+end
 
