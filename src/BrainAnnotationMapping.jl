8ae08334b28b781b91b4384f81ed93adb18b1c92
Donghoon Lee <mdhe1248@gmail.com>
2023-03-23 22:22:07 -0400

    blobpos type updated

--- src/BrainAnnotationMapping.jl
+++ src/BrainAnnotationMapping.jl
@@ -1,7 +1,7 @@
 module BrainAnnotationMapping
 using Statistics, Images
 
-export retrieve, isdownstream, label_points, filter_points, count_cells, brainmapping, saveData, load_brainmap_json, annotation_boundary, appendCol!, appendVecCol!, getPosCounts, getIntensityMean, getNPixels, imshow_attn, selectArea, blob_filter, findnearest, detect_blobs, scale_pos, blobs2df, loadimgs, load_pos_in_physical_space, imcat
+export retrieve, isdownstream, label_points, filter_points, count_cells, brainmapping, saveData, load_brainmap_json, annotation_boundary, appendCol!, appendVecCol!, getPosCounts, getIntensityMean, getNPixels, imshow_attn, selectArea, blob_filter, findnearest, detect_blobs, scale_pos, blobs2df, loadimgs, load_pos_in_physical_space, imcat, BlobPos
 # Write your package code here.
 
 include("annotationmapping_idisco.jl")
