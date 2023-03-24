fe9afe7ae2acaeb3502a9391a230532751afd63d
Donghoon Lee <mdhe1248@gmail.com>
2023-03-23 22:19:48 -0400

    using Images

--- src/BrainAnnotationMapping.jl
+++ src/BrainAnnotationMapping.jl
@@ -1,5 +1,5 @@
 module BrainAnnotationMapping
-using Statistics
+using Statistics, Images
 
 export retrieve, isdownstream, label_points, filter_points, count_cells, brainmapping, saveData, load_brainmap_json, annotation_boundary, appendCol!, appendVecCol!, getPosCounts, getIntensityMean, getNPixels, imshow_attn, selectArea, blob_filter, findnearest, detect_blobs, scale_pos, blobs2df, loadimgs, load_pos_in_physical_space, imcat
 # Write your package code here.
