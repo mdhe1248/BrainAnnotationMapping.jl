0347800a704858ef45aac3c8145493e6e82a972d
Donghoon Lee <mdhe1248@gmail.com>
2023-03-26 18:58:18 -0400

    add OffsetArrays

--- src/BrainAnnotationMapping.jl
+++ src/BrainAnnotationMapping.jl
@@ -1,5 +1,5 @@
 module BrainAnnotationMapping
-using Statistics, Images
+using Statistics, Images, OffsetArrays
 
 export retrieve, isdownstream, label_points, filter_points, count_cells, brainmapping, saveData, load_brainmap_json, annotation_boundary, appendCol!, appendVecCol!, getPosCounts, getIntensityMean, getNPixels, imshow_attn, selectArea, blob_filter, findnearest, detect_blobs, scale_pos, blobs2df, loadimgs, load_pos_in_physical_space, loaddfs, imcat, BlobPos
 # Write your package code here.
